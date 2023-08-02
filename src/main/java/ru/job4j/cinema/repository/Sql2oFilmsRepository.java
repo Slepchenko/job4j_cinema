fc43905b12b22ea1da4994086ada49d61e2bc2a5
Alexander Slepchenko <asander@list.ru>
2023-08-02 22:21:53 +0300

    Добавление репозиториев

---
+++ src/main/java/ru/job4j/cinema/repository/Sql2oFilmsRepository.java
@@ -0,0 +1,18 @@
+package ru.job4j.cinema.repository;
+
+import ru.job4j.cinema.model.Film;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class Sql2oFilmsRepository implements Films {
+    @Override
+    public Optional<Film> findById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Collection<Film> findAll() {
+        return null;
+    }
+}
